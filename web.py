4ccb832c9a19689fdc9f808ae3127a49a5cf1e7d
pi <alber91@gmail.com>
2023-10-08 16:02:18 +0000

    Updated web

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ from huawei_lte_api.enums.client import ResponseEnum
 import requests
 import urllib3
 import os
+import re
 
 urllib3.disable_warnings()
 user = os.getenv('API_USER')
@@ -40,14 +41,46 @@ def restore_dns():
         ui.notify(str(was))
     session.close()
 
+def fix_val(value, regex, max, offset, invert):
+    rex = re.search(regex, value)
+    if invert:
+        fixed_val = (max - float(rex.group(1)) + offset)/max
+    else:
+        fixed_val = float(rex.group(1))/max
+    return fixed_val
+
+def get_color(val):
+    if (val*100 > 50):
+        color = "green"
+    elif (val*100 > 25):
+        color = "yellow"
+    else:
+        color = "red"
+    return color
+
 def get_info():
     session = create_session()
     with Connection(url, requests_session=session) as connection:
         client = Client(connection) # This just simplifies access to separate API groups, you can use device = Device(connection) if you want
-        ui.notify(str(client.device.signal()))  # Can be accessed without authorization
+        signal = client.device.signal()
+        ui.notify(str(signal))  # Can be accessed without authorization
         ui.notify(str(client.device.information()))  # Needs valid authorization, will throw exception if invalid credentials are passed in URL
         ui.notify(str(client.dhcp.settings()))
     session.close()
+
+    val = fix_val(signal["rsrq"], "-(\d+\.\d+)dB", 20, 0, True)
+    color = get_color(val)
+    i.style('color: ' + color)
+    i.props('color=' + color)
+    i.set_value(val)
+    i.tooltip(str(signal["rsrq"]))
+
+    val = fix_val(signal["rsrp"], "-(\d+)dBm", 100, 60, True)
+    color = get_color(val)
+    j.style('color: ' + color)
+    j.props('color=' + color)
+    j.set_value(val)
+    j.tooltip(str(signal["rsrp"]))
 
 def reboot_router():
     session = create_session()
@@ -67,5 +100,11 @@ with ui.column().classes('fixed-center').style('align-items: center;'):
         ui.button('Restore DNS', on_click=lambda: [restore_dns()])
         ui.button('Get info', on_click=lambda: [get_info()])
         ui.button('Reboot', on_click=lambda: [reboot_router()])
+        with ui.row().style('align-items: center;'):
+            ui.label("RSRQ")
+            ui.label("RSRP")
+        with ui.row().style('align-items: center;'):
+            i = ui.circular_progress()
+            j = ui.circular_progress()
 
 ui.run(port=2233, title="RouterConfig", dark=None, reload=False)
